5f46ab709cc1b1f318a8831ab0523476f305ce6d
Thomas Gumede <gumedethomas@outlook.com>
2024-09-11 22:46:42 +0200

    admin events

--- events/admin.py
+++ events/admin.py
@@ -18,7 +18,7 @@ def make_pending(modeladmin, request, querset):
 
 class EventTicketTypeInline(admin.TabularInline):
     model = EventTicketTypeModel
-    
+    readonly_fields = ("sale_start", "sale_end", "event")
     extra = 0
 
 class TicketOrderInline(admin.TabularInline):
