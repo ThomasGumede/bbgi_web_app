57e8641e8fb4ee78e6ff8fb15106e65eb1ab2acc
Thomas Gumede <gumedethomas@outlook.com>
2025-07-14 16:04:34 +0200

    check v2

--- bbgi/settings.py
+++ bbgi/settings.py
@@ -288,8 +288,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'mail.bbgi.co.za'
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'noreply@bbgi.co.za'
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
