48f7ef074823f5fe38af40f44daf6b607d12550c
Thomas Gumede <gumedethomas@outlook.com>
2025-07-20 16:22:17 +0200

    h

--- bbgi/settings.py
+++ bbgi/settings.py
@@ -287,9 +287,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'mail.bbgi.co.za'
+EMAIL_HOST = 'mail.siyazalana.org'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'noreply@bbgi.co.za'
+EMAIL_HOST_USER = 'noreply@siyazalana.org'
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = 'noreply@bbgi.co.za'
