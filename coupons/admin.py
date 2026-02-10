92f4505e67ddce9b4060991cc47aeddcd79febe9
Thomas Gumede <gumedethomas@outlook.com>
2026-02-10 20:42:12 +0200

    admin

--- coupons/admin.py
+++ coupons/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from coupons.models import Coupon
 
+@admin.action(description="Deactivate selected coupons")
+def make_inactive(modeladmin, request, querset):
+    querset.update(active=False)
+
 @admin.register(Coupon)
 class CouponAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("code", "discount", "valid_from", "valid_to", "active")
+    list_filter = ("active", "valid_from", "valid_to")
+    actions = [make_inactive]
